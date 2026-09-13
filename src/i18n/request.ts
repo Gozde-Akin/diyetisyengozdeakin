722b37183a9a09c693514b61d811eda7cc9f51a3
TVK Group / TVK Labs & Technologies <team@tvklabs.com>
2026-09-13 11:30:42 +0300

    Apply private-clinic content overrides across all locales

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,6 @@
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
+import { applyClinicMessageOverrides } from "./clinic-messages";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
@@ -8,8 +9,10 @@ export default getRequestConfig(async ({ requestLocale }) => {
     locale = routing.defaultLocale;
   }
 
+  const messages = (await import(`./messages/${locale}.json`)).default;
+
   return {
     locale,
-    messages: (await import(`./messages/${locale}.json`)).default,
+    messages: applyClinicMessageOverrides(locale, messages),
   };
 });
